32e8b48f73e2430d196dbdf934a29840b56aa9c9
tyler :3 <tyler@shrapnelnet.co.uk>
2023-11-28 21:21:59 +0000

    implement pagination

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import PostSkeleton from "./components/PostSkeleton.jsx"
 import Post from "./components/Post.jsx"
 import { Box } from "@mui/material"
 import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import CircularProgress from "@mui/material/CircularProgress"
 
 export default function App({isLoggedIn}) {
     const [loading, setLoading] = useState(true)
     const [posts, setPosts] = useState([])
     const [offset, setOffset] = useState(0)
+    const [paginateLoading, setPaginateLoading] = useState(false)
+    const [noMorePosts, setNoMorePosts] = useState(false)
 
     useEffect(() => {
         fetch("/api/getposts", {
@@ -18,11 +22,17 @@ export default function App({isLoggedIn}) {
         })
             .then((res) => res.json())
             .then((res) => {
-                setPosts(res)
+                if (res.length < 10) {
+                    setNoMorePosts(true)
+                    if (res.length === 0)
+                        return
+                }
+                setPosts([...posts, ...res])
                 setOffset(offset + 10)
                 setLoading(false)
+                setPaginateLoading(false)
             })
-    }, [])
+    }, [paginateLoading])
 
     return (
         <React.Fragment>
@@ -45,8 +55,17 @@ export default function App({isLoggedIn}) {
                     :
                         null
             }
-            <Box>
-                <Button variant={"contained"}>Load More</Button>
+            <Box sx={{ display: "flex", justifyContent: "center", padding: "20px 0 100px 0" }}>
+                {
+                    noMorePosts
+                    ?
+                        <Typography>So this is the end...</Typography>
+                    :
+                        <React.Fragment>
+                            <Button onClick={() => {setPaginateLoading(true)}} variant={"contained"}>Load More</Button>
+                            <CircularProgress sx={{ marginLeft: "20px", display: paginateLoading ? "block": "none" }} />
+                        </React.Fragment>
+                }
             </Box>
         </React.Fragment>
     )
